a4c0f6c65ffd9c932b15f88013854012b6999f79
Saad Iqbal <63910739+SaadIqbalGH@users.noreply.github.com>
2021-10-05 13:01:56 +0500

    Update #1.Read Account balance with Web3.js

--- #1.Read Account balance with Web3.js
+++ #1.Read Account balance with Web3.js
@@ -1,6 +1,9 @@
-const Web3 = require('web3')
-const rpcURL = 'https://mainnet.infura.io/v3/21789368dc1f4df8bba7e3913ddf354a'
-const web3 = new Web3(rpcURL)
-const address = '0x6D7C96BA0689fe281F0EEF51a7173eC621aF2A32'
-web3.eth.getBalance(address, (err, wei) => {balance = web3.utils.fromWei(wei, 'ether')})
-
+require ('dotenv').config();
+const Web3 = require('web3');
+const rpcURL = process.env.INFURA_MAINNET_URL;
+const web3 = new Web3(rpcURL);
+const address = '0x6D7C96BA0689fe281F0EEF51a7173eC621aF2A32';
+web3.eth.getBalance(address, (err, wei) => {
+    const balance = web3.utils.fromWei(wei, 'ether'); 
+    console.log(balance);} 
+);
